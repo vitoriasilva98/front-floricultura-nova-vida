e13bdeac8ca47f7fcf5817e9d35657c371680d4d
Vitória da Silva <vitoriasilva98@hotmail.com>
2022-07-24 09:12:46 -0300

    :construction: Desenvolvendo interface mobile (phone)

---
+++ public/funcionalidade.js
@@ -0,0 +1,21 @@
+
+var imgs = ["img/sobre-nos-dente-de-leao.png", "img/sobre-nos-flor.png", "img/sobre-nos-planta.png", "img/sobre-nos-lavanda.png"];
+var indice = 1;
+
+document.getElementById('sobre__nos_img').src = imgs[0];
+setInterval(contagem, 5000);
+
+function contagem() {
+    document.getElementById('sobre__nos_img').src = imgs[indice];
+    indice++;
+
+    if (indice > 3) {
+        indice = 0;
+    }
+}
+
+
+
+
+
+
